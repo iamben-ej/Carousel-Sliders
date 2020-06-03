ebf1cabca4356dcb8503b88c26e88a2401426ffa
iamben-ej <ejben55@gmail.com>
2020-06-03 01:42:29 +0100

    Infinite transition done for 'next' button.

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const slider = document.querySelector(".slider");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 
+//next button
 next.addEventListener("click", function () {
   slider.style.transform = "translate(-20%)";
+});
+
+//move already transitioned element to end of elements.
+slider.addEventListener("transitionend", function () {
+  slider.appendChild(slider.firstElementChild);
+
+  //reset translate (back) to 0 to make next content the current first element.
+  slider.style.transition = "none";
+  slider.style.transform = "translate(0)";
+  setTimeout(function () {
+    slider.style.transition = "all 0.5s";
+  });
 });
