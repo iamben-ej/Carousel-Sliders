20711e3696ac8f73081489f132b6331825381544
iamben-ej <ejben55@gmail.com>
2020-06-07 17:21:26 +0100

    Infinite carousel done.

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const slider = document.querySelector(".slider");
+const carousel = document.querySelector(".carousel");
 
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 
+var direction;
+
+//prev button
+prev.addEventListener("click", function () {
+  if (direction = -1) {
+    //slider.appendChild(slider.firstElementChild);
+    direction = 1;
+  }
+  carousel.style.justifyContent = "flex-end";
+  slider.style.transform = "translate(20%)";
+});
+
 //next button
 next.addEventListener("click", function () {
+  direction = -1;
+  carousel.style.justifyContent = "flex-start";
   slider.style.transform = "translate(-20%)";
 });
 
 //move already transitioned element to end of elements.
 slider.addEventListener("transitionend", function () {
-  slider.appendChild(slider.firstElementChild);
+  if (direction == -1) {
+    slider.appendChild(slider.firstElementChild);
+  } else if (direction == 1) {
+    slider.prepend(slider.lastElementChild);
+  }
 
   //reset translate (back) to 0 to make next content the current first element.
   slider.style.transition = "none";
