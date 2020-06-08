4154289a53e1676ee84a6e9f5a4abe16057f47e4
iamben-ej <ejben55@gmail.com>
2020-06-08 02:07:39 +0100

    next and prev button function.

--- script.js
+++ script.js
@@ -1,51 +1,14 @@
-const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 
-const slider = document.querySelector('.slider');
-const carousel = document.querySelector('.carousel');
+const track = document.querySelector('.track');
 
-var sectionIndex = 0;
+const carouselWidth = document.querySelector('.carousel-container').offsetWidth;
 
-function reset() {
-  for (var i = 0; i < slider.children.length; i++) {
-    slider.children[i].style.opacity = 0;
-    slider.children[i].style.zIndex = 0;
-  }
-}
-
-var intervalId = 0;
-
-//Auto-fade function ######################################################
-function startShow() {
-  intervalId = setInterval(function () {
-    reset();
-    sectionIndex = sectionIndex < 4 ? sectionIndex + 1 : 0;
-    slider.children[sectionIndex].style.zIndex = 1;
-    slider.children[sectionIndex].style.opacity = 1;
-  }, 5000);
-}
-
-startShow();
-carousel.addEventListener('mouseover', function () {
-  clearInterval(intervalId);
+next.addEventListener('click', () => {
+    track.style.transform = `translate(-${carouselWidth}px)`;
 });
 
-carousel.addEventListener('mouseout', function () {
-  startShow();
-});
-
-//Next button ######################################################
-next.addEventListener('click', function () {
-  reset();
-  sectionIndex = sectionIndex < 4 ? sectionIndex + 1 : 4;
-  slider.children[sectionIndex].style.zIndex = 1;
-  slider.children[sectionIndex].style.opacity = 1;
-});
-
-//Previous button ######################################################
-prev.addEventListener('click', function () {
-  reset();
-  sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
-  slider.children[sectionIndex].style.zIndex = 1;
-  slider.children[sectionIndex].style.opacity = 1;
+prev.addEventListener('click', () => {
+    track.style.transform = `translate(-${0}px)`;
 });
